bcf1453e9c55649e1f308a193123b9555a48b5cf
enoD <enod2008@gmail.com>
2015-04-19 03:30:22 +0800

    fexed1

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -8,7 +8,18 @@ class Receiver < ActiveRecord::Base
 	validates_presence_of :to
 	validates_presence_of :user_id
 	belongs_to :user
-	has_attached_file :image
+	has_attached_file :image,
+	:style => { :medium => "300x300>", :thumb => "100x100>" },
+	:storage => :s3,
+	:bucket  => ENV['MY_BUCKET_NAME'],
+	:s3_credentials => {
+		:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+		:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+		},
+		:s3_permissions => 'private',
+		:url => ":s3_domain_url"
+		validates :image, presence: true
+		validates :description, presence: true
 
 	validates_attachment_content_type :image, 
 	content_type:  /^image\/(png|gif|jpeg)/,
